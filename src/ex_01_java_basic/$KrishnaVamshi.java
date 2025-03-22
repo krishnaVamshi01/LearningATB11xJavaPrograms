f5b824da31d594c3ee1dbf1d1a07316e9f163e8c
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-22 18:44:06 +0530

    Examples - Rules for Naming convention

---
+++ src/ex_01_java_basic/$KrishnaVamshi.java
@@ -0,0 +1,7 @@
+package ex_01_java_basic;
+
+public class $KrishnaVamshi {
+    public static void main(String[] args) {
+        System.out.println("$KrishnaVamshi");
+    }
+}
---
+++ src/ex_01_java_basic/NamingConvention.java
@@ -0,0 +1,13 @@
+package ex_01_java_basic;
+
+public class NamingConvention {
+//    Start with a "Capital-letter"
+//    for example KrishnaVamshi
+public static void main(String[] args) {
+    System.out.println("KrishnaVamshi");
+//    Never use "space"
+//    never start with .
+//    never leave empty
+//    never start with an integer
+}
+}
