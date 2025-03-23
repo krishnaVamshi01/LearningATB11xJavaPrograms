052b59337cdc00b27f9198a61fea1ae34eb34843
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-23 10:28:25 +0530

    Pushing "Hello world " to GIT

--- src/EX_01_java_basic/HelloWorld.java
+++ src/EX_01_java_basic/HelloWorld.java
@@ -1,4 +1,4 @@
-package ex_01_java_basic;
+package EX_01_java_basic;
 
 public class HelloWorld {
     public static void main(String[] args) {
--- src/EX_01_java_basic/Lab001_HelloWorldProgram.java
+++ src/EX_01_java_basic/Lab001_HelloWorldProgram.java
@@ -1,4 +1,4 @@
-package ex_01_java_basic;
+package EX_01_java_basic;
 
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
