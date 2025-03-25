106f05a97cb973d494a8049ffaca83dedfecc35b
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-25 12:17:39 +0530

    AI - basics

--- src/EX_01_java_basic/LabAI01.java
+++ src/EX_01_java_basic/LabAI01.java
@@ -2,6 +2,8 @@ package EX_01_java_basic;
 
 public class LabAI01 {
     public static void main(String[] args) {
+        //using AI ,
+        //AI writes the code by taking some inputs
 
     }
 }
