6f2b23bfd48b5f4f4388ef84fd224ef9645732f2
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-25 12:13:21 +0530

    Keywords - Identifers - Print-Println

--- src/EX_01_java_basic/Lab012_Common_Math_Exception.java
+++ src/EX_01_java_basic/Lab012_Common_Math_Exception.java
@@ -3,8 +3,9 @@ package EX_01_java_basic;
 public class Lab012_Common_Math_Exception {
     public static void main(String[] args) {
 
-        System.out.println(20%0);
+        System.out.println(20/0);
 //        Exception in thread "main" java.lang.ArithmeticException: / by zero
+//        you cant divide divide integer with 0
 
     }
 }
