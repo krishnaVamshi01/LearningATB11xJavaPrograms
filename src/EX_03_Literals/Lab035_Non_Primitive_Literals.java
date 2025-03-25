b0cd795a92a7c089a21a1e317517292f0b51b7c5
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-25 12:17:07 +0530

    Literals - Types of Literals - Prmitive - Non-Primitives

--- src/EX_03_Literals/Lab035_Non_Primitive_Literals.java
+++ src/EX_03_Literals/Lab035_Non_Primitive_Literals.java
@@ -11,12 +11,19 @@ public class Lab035_Non_Primitive_Literals {
         //Non-primitive (defined by user), reference Data types
         // no size  --> no max or min defined
         //Byte - 8, bits
-        String name = "Sushma"; // String here is a bunch of characters
+        String name = "Krishna"; // String here is a bunch of characters
         int[] arrays_of_items = new int[10];
+//        arr is non-primitive (reference type) → Java prints a memory reference, not the elements.
+//        2. Understanding [I@6f496d9f
+//[I → Integer Array (I stands for int).
+//@6f496d9f → Memory hashcode (not the actual address).
+//Java does not print array elements directly.
 
 //        System.out.println(arrays_of_items); When printed this out put was [I@6f496d9f
 
         // String, Arrays, Classes, enum etc..
+        System.out.println(name);
+        System.out.println(arrays_of_items);
 
     }
 }
