a1f9e31be4cb9e5439a4c7416122e6ed9b6d593f
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-25 19:12:00 +0530

    Math.functions

---
+++ src/EveryDay_Tasks/march_12th_Math_func.java
@@ -0,0 +1,14 @@
+package EveryDay_Tasks;
+
+
+public class march_12th_Math_func {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+        int z = 10;
+        //math.functions
+       double result = Math.cbrt(Math.pow(x,2)+Math.pow(y,2)-Math.abs(z));
+        System.out.println(result);
+
+    }
+}
