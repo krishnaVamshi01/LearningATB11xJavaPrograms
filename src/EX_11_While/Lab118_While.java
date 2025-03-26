b7d55580efd82ed5b8ec4dabc8e4646f6257d0a9
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-26 20:08:19 +0530

    While Loops

---
+++ src/EX_11_While/Lab118_While.java
@@ -0,0 +1,10 @@
+package EX_11_While;
+
+public class Lab118_While {
+    public static void main(String[] args) {
+//        we should intialize otherwise infinite loop
+        while (true){
+            System.out.println("Hello, Pramod");
+        }
+    }
+}
---
+++ src/EX_11_While/Lab120_While_Check.java
@@ -0,0 +1,11 @@
+package EX_11_While;
+
+public class Lab120_While_Check {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 0) { //false condtion
+            System.out.println(i);
+            i++;
+        }
+    }
+}
