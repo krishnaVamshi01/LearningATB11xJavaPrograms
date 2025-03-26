0040fc68c5ecc3e2529e57f3dd25dd0771a781b5
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-26 18:44:54 +0530

    Switch - Break Statements

---
+++ src/EX_09_Switch/Lab088_INTERVIEW.java
@@ -0,0 +1,9 @@
+package EX_09_Switch;
+
+public class Lab088_INTERVIEW {
+    public static void main(String[] args) {
+        int a = 10;
+        switch (a) {
+        }
+    }
+}
---
+++ src/EX_09_Switch/Lab091_Interview.java
@@ -0,0 +1,10 @@
+package EX_09_Switch;
+
+public class Lab091_Interview {
+    public static void main(String[] args) {
+        long a11 = 30l;
+        switch ((int) a11) {
+        }
+
+    }
+}
