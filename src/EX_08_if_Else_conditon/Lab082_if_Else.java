00c1e6f68669f1eab26444d549188c76da14a26c
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-26 18:11:15 +0530

    If-Else - conditions

---
+++ src/EX_08_if_Else_conditon/Lab082_if_Else.java
@@ -0,0 +1,29 @@
+package EX_08_if_Else_conditon;
+
+import java.util.Scanner;
+
+public class Lab082_if_Else {
+    public static void main(String[] args) {
+        // Allowed to vote or not
+        // If age > 18 -> allowed to vote.
+        // else age < >18 -> Not allowed to vote.
+
+        // How to take the user Input
+        // 1. CLI Options
+        //
+//        int age = Integer.parseInt(args[0]);
+
+        // 2. Scanner Class
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age!");
+        int age = scanner.nextInt();
+        System.out.println(age);
+
+
+        if (age > 18) {
+            System.out.println("Allowed to vote!");
+        } else {
+            System.out.println("Not Allowed!");
+        }
+    }
+}
