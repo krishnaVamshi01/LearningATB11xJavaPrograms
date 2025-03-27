c37dcda4ce49e4b148e6548e37a3e3e74096c257
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-27 09:06:34 +0530

    Functions

---
+++ src/EX_13_Functions/Lab133_InterviewQ.java
@@ -0,0 +1,27 @@
+package EX_13_Functions;
+
+public class Lab133_InterviewQ {
+    public static void main(String[] args) {
+        main(3.14f);
+        System.out.println(main(3.14f));  // Prints 10
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main(int args) {
+        return 10;
+    }
+
+    public static int main(float args) {
+        return 20;
+    }
+
+    public static boolean main(boolean args) {
+        return true;
+
+    }
+    }
+
