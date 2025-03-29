a72a667eff1a79c8c447e50a1d3c8f30f646eb10
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-03-29 19:39:10 +0530

    Arrays

---
+++ src/EX_16_Arrays/Lab151_Arrays_Part2.java
@@ -0,0 +1,29 @@
+package EX_16_Arrays;
+
+public class Lab151_Arrays_Part2 {
+    public static void main(String[] args) {
+        String[] names =  {"Krishna","amit","lucky"};
+        System.out.println(names.length);
+        System.out.println(names[0]);
+        System.out.println(names[2]);
+        System.out.println(names[1]);
+
+
+        String[] name_atb11x = new String[3];
+        name_atb11x[0] = "Aditi";
+        name_atb11x[1] = "Jitin";
+        name_atb11x[2] = "Nikita";
+        System.out.println(name_atb11x.length);
+        System.out.println(name_atb11x[0]);
+        System.out.println(name_atb11x[2]);
+        System.out.println(name_atb11x[1]);
+
+        boolean[] is_male_data = new boolean[2];
+        is_male_data[0] =  true;
+        is_male_data[1] =  false;
+
+        System.out.println(is_male_data[0]);
+
+
+    }
+}
---
+++ src/EX_16_Arrays/Lab158_String_CLI.java
@@ -0,0 +1,11 @@
+package EX_16_Arrays;
+
+public class Lab158_String_CLI {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length ; i++) {
+            System.out.println(args[i]);
+        }
+
+        // we have not specified length of array in cli option
+    }
+}
