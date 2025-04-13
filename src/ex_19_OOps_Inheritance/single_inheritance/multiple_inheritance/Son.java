6bf1d0ed368cf8fde3d3b7255cea229b9cce8350
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-04-13 16:40:41 +0530

    Inheritence - single - multilevel - multiple

--- src/ex_19_OOps_Inheritance/single_inheritance/multiple_inheritance/Son.java
+++ src/ex_19_OOps_Inheritance/single_inheritance/multiple_inheritance/Son.java
@@ -2,7 +2,7 @@ package ex_19_OOps_Inheritance.single_inheritance.multiple_inheritance;
 
 public class Son extends Mother_MP//Father_MP{
 {
-//you can inherit only from one
+
     // You can't have multiple inheritance
     // You can't take from multiple people - classes
 
