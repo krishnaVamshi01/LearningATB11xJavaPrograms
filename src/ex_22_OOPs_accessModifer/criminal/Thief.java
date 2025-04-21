19779e69b9e7a7b726eb504ad9933552ab84ea10
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-04-21 15:51:21 +0530

    Encapsulation - Access Modifier

---
+++ src/ex_22_OOPs_accessModifer/criminal/Thief.java
@@ -0,0 +1,13 @@
+package ex_22_OOPs_accessModifer.criminal;
+
+import ex_22_OOPs_accessModifer.police.Cop;
+
+public class Thief {
+    public static void main(String[] args) {
+
+            Cop theif = new Cop(100);
+            //theif.canIShoot();
+            //System.out.println(theif.gun);
+        }
+
+    }
