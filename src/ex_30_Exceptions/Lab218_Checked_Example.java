e32a6e6a3e7d57582b5265f1dda436e442d297e3
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-05-01 19:57:27 +0530

    primitive type - Wrapper Class - Exception - Handling

---
+++ src/ex_30_Exceptions/Lab218_Checked_Example.java
@@ -0,0 +1,12 @@
+package ex_30_Exceptions;
+
+import java.io.FileInputStream;
+
+public class Lab218_Checked_Example {
+    public static void main(String[] args) {
+
+        // Checked Exception
+//        FileInputStream fileInputStream = new FileInputStream("C://testdata.txt");
+
+    }
+}
