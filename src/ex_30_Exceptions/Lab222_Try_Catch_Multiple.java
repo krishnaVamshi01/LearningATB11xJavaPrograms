9e08bc7b1bc4b8541c5be985362d3e240d976b9c
Krishna Vamshi <vodnalakrishnavamshi1@gmail.com>
2025-05-02 12:00:51 +0530

    Exception - Handling - Try - Catch - Finally - Throw - Throws

---
+++ src/ex_30_Exceptions/Lab222_Try_Catch_Multiple.java
@@ -0,0 +1,18 @@
+package ex_30_Exceptions;
+
+public class Lab222_Try_Catch_Multiple {
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("ARE YOU FOOL");
+        } catch (Exception e) {
+            System.out.println("ARE YOU FOOL 2");
+        }
+        System.out.println("2");
+
+
+    }
+}
---
+++ src/ex_30_Exceptions/Lab233_Throw.java
@@ -0,0 +1,14 @@
+package ex_30_Exceptions;
+
+public class Lab233_Throw {
+    public static void main(String[] args) {
+
+    }
+
+    static void divideByZero(int a) {
+        // check some
+        if (a == 0) {
+            throw new ArithmeticException();
+        }
+    }
+}
